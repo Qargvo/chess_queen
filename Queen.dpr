0015fd035f1bef2445c87913de5aca09d5e9d7af
Qargvo <47695656+Qargvo@users.noreply.github.com>
2019-03-01 20:10:27 +0300

    initialization

--- Queen.dpr
+++ Queen.dpr
@@ -11,8 +11,28 @@ var
   ch: techer;
   log: array of Byte;
   i, j: Byte;
+  n, m,t: Byte;
+  cnt: Byte;
+
+procedure incarr(var arr: techer; x, y: byte);
+var
+  i: Byte;
   n, m: Byte;
-  cnt:Byte;
+begin
+n:=Length(arr)-1;
+m:=Length(arr[0])-1;
+for i:=0 to m do
+  arr[]
+
+end;
+
+procedure decarr(var arr: techer; x, y: byte);
+var
+  i: Byte;
+  n, m: Byte;
+begin
+
+end;
 begin
   {Write('Enter the number of measurements: ');
   Readln;
@@ -22,17 +42,18 @@ begin
   Readln(n, m);
   if m > n then
   begin
-    m := n + m;
-    n := m - n;
-    m := m - n;
+    t := m;
+    m := n;
+    n :=t ;
   end;
   SetLength(ch, n, m);
   SetLength(log, m);
-  for i:=0 to m-1 do
-    log[i]:=0;
-  for i:=0 to n-1 do
-    for j:=0 to m-1 do
-      ch[i,j]:= 0;
+  for i := 0 to m - 1 do
+    log[i] := 0;
+  for i := 0 to n - 1 do
+    for j := 0 to m - 1 do
+      ch[i, j] := 0;
+
   Readln;
 end.
  
