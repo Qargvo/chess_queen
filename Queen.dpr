e934f2cc5576b858899e2b6a3e18ef1de9d02bf2
V <qargvo@gmail.com>
2019-03-02 19:08:47 +0300

    beta

--- Queen.dpr
+++ Queen.dpr
@@ -9,8 +9,8 @@ type
   techer = array of array of byte;
 var
   ch: techer;
-  log: array of Byte;
-  i, j: Byte;
+  log: array of ShortInt;
+  i, j: shortint;
   n, m, t: Byte;
   cnt: Byte;
 
@@ -105,27 +105,79 @@ begin
   end;
 
 end;
+
+procedure Write_Mas(const mas: techer);
+var
+  i, j: cardinal;
+begin
+  for i := Length(mas) - 1 downto 0 do
+  begin
+    for j := 0 to Length(mas[i]) - 1 do
+      if mas[i, j] = 6 then
+        write('#')
+      else
+        write('+');
+    Writeln;
+  end;
+  writeln;
+end;
 begin
   {Write('Enter the number of measurements: ');
   Readln;
   writeln('Let there be 2 measurements');}
-
-  Write('Enter size of desck(n,m): ');
-  Readln(n, m);
-  if m > n then
+  while True do
   begin
-    t := m;
-    m := n;
-    n := t;
+    Write('Enter size of desck(n,m): ');
+    cnt := 0;
+    Readln(n, m);
+    if m > n then
+    begin
+      t := m;
+      m := n;
+      n := t;
+    end;
+    SetLength(ch, n, m);
+    SetLength(log, m);
+    for i := 0 to m - 1 do
+      log[i] := -1;
+    for i := 0 to n - 1 do
+      for j := 0 to m - 1 do
+        ch[i, j] := 0;
+    i := 0;
+    j := 0;
+    while j >= 0 do
+    begin
+      i := log[j];
+      inc(i);
+      if i = n then
+      begin
+        if j = 0 then
+          Break;
+        log[j] := -1;
+        Dec(j);
+        decarr(ch, log[j], j);
+        Continue;
+      end;
+      if ch[i, j] <> 0 then
+      begin
+        log[j] := i;
+        Continue;
+      end;
+      incarr(ch, i, j);
+      log[j] := i;
+      Inc(j);
+      if j = m then
+      begin
+        Inc(cnt);
+        dec(j);
+        Write_Mas(ch);
+        decarr(ch, log[j], j);
+        Continue;
+      end;
+
+    end;
+    Writeln(cnt);
   end;
-  SetLength(ch, n, m);
-  SetLength(log, m);
-  for i := 0 to m - 1 do
-    log[i] := 0;
-  for i := 0 to n - 1 do
-    for j := 0 to m - 1 do
-      ch[i, j] := 0;
-  
   Readln;
 end.
 
